fbb3d952819855e6cf81df713a833ddc5e527684
Naveen VJ <vjnaveen2005@gmail.com>
2025-01-16 21:12:16 +0530

    Add Babel plugin for private property support and remove rehype-highlight import from MarkdownRender

--- src/components/MarkdownRender.jsx
+++ src/components/MarkdownRender.jsx
@@ -3,7 +3,6 @@ import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import remarkGfm from "remark-gfm";
-import rehypeHighlight from "rehype-highlight";
 
 const MarkdownRender = (props) => {
 
