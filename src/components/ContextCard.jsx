625fc1df0c8ac08f8b66b3da105b4496c517f444
Naveen VJ <vjnaveen2005@gmail.com>
2025-01-16 18:32:00 +0530

    Update ContextCard component to adjust text size for better readability on different screen sizes

--- src/components/ContextCard.jsx
+++ src/components/ContextCard.jsx
@@ -9,7 +9,7 @@ const ContextCard = ({ title, content, link }) => {
         <div className="border-2 border-black dark:border-none bg-white w-40 h-28 md:h-44 md:w-96 p-2 md:p-5 rounded-2xl m-2 md:m-5 cursor-pointer">
           <h1 className="text-sm md:text-lg font-bold">{title}</h1>
           <hr className="border-solid border-black" />
-          <p className="text-[8px] md:text-base">{content}</p>
+          <p className="text-xs md:text-base">{content}</p>
         </div>
       </NavLink>
     </motion.div>
